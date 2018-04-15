78773aa166522a30042115dfce6940dc97966188
julientregoat <julienctregoat@gmail.com>
2018-04-15 00:39:55 -0400

    added a LIL more color

--- src/components/PostPreview.js
+++ src/components/PostPreview.js
@@ -3,7 +3,7 @@ import { List } from 'semantic-ui-react'
 
 const PostPreview = (props) => (
   <List.Item onClick={() => window.open(props.link)}className="post-preview">
-    <h3>{props.title}</h3>
+    <h3><a>{props.title}</a></h3>
     <p>{props.date}</p>
   </List.Item>
 );
